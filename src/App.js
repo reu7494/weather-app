25be8e981234b7232e78636075bfc64c6086c759
black-ne-ko <reu7494@naver.com>
2025-06-17 12:23:04 +0900

    데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import { useState, useEffect } from "react";
+import WeatherBox from "./WeatherBox";
 
 export default function App() {
   const [location, setLocation] = useState("");
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    } else {
-      setLocation("Geolocation is not supported by this browser.");
+  useEffect(() => {
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+      } else {
+        setLocation("Geolocation is not supported by this browser.");
+      }
     }
-  }
 
-  function success(position) {
-    setLocation(
-      `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`
-    );
-  }
+    function success(position) {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      setLocation(`Latitude: ${lat}, Longitude: ${lon}`);
+      getWeather(lat, lon);
+    }
 
-  function error() {
-    setLocation("Sorry, no position available.");
-  }
+    function error() {
+      setLocation("Sorry, no position available.");
+    }
+
+    async function getWeather(lat, lon) {
+      const apiKey = process.env.REACT_APP_WEATHER_KEY;
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+    }
+
+    getLocation();
+  }, []);
 
   return (
     <>
-      <h2>현재 위치</h2>
-      <button onClick={getLocation}>Try It</button>
+      <WeatherBox />
       <p>{location}</p>
     </>
   );
---
+++ src/WeatherBox.js
@@ -0,0 +1,9 @@
+export default function WeatherBox() {
+  return (
+    <div className="weather-box">
+      <h2 className="city">서울</h2>
+      <h3>온도 / 시간</h3>
+      <h4>맑음</h4>
+    </div>
+  );
+}
