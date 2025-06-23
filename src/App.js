d5f2892ff1e88b55327c0b1754a38a82d9c7735a
black-ne-ko <reu7494@naver.com>
2025-06-23 14:19:11 +0900

    아이콘 안보임

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { useState, useEffect } from "react";
-import WeatherBox from "./WeatherBox";
+import { WeatherBox } from "./WeatherBox";
+import { WeatherDescKo } from "./WeatherDescKo";
 
 export default function App() {
-  const [weatherData, setWeatherData] = useState();
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    temperature: 0,
+    weatherIconUrl: undefined,
+    description: "",
+  });
+
+  const API_KEY = process.env.REACT_APP_WEATHER_KEY;
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -11,17 +19,33 @@ export default function App() {
       getWeather(lat, lon);
     });
   }, []);
+
   const getWeather = async (lat, lon) => {
-    const apiKey = process.env.REACT_APP_WEATHER_KEY;
     try {
       const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=ko`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
       );
       const data = await res.json();
-      setWeatherData(data);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
+      const location = data.name;
+
+      const temperature = Math.round(data.main.temp);
+
+      const weatherIcon = data.weather[0].icon;
+      const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+
+      const weatherId = data.weather[0].id;
+      const description = WeatherDescKo[weatherId];
+
+      setWeatherData({
+        location,
+        temperature,
+        weatherIconUrl,
+        description,
+      });
+      console.log("weatherId:", weatherId);
+      console.log("description from WeatherDescKo:", WeatherDescKo[weatherId]);
+    } catch (err) {
+      console.log(err);
     }
   };
 
--- src/WeatherBox.js
+++ src/WeatherBox.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function WeatherBox({ weatherData }) {
+export function WeatherBox({ weatherData }) {
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
@@ -14,7 +14,7 @@ export default function WeatherBox({ weatherData }) {
   return (
     <div className="weather-box">
       <div className="weather-information">
-        <h2 className="city">{weatherData?.name}</h2>
+        <h2 className="city">{weatherData?.location}</h2>
         <h2>
           {currentTime.toLocaleDateString({
             year: "numeric",
@@ -36,23 +36,9 @@ export default function WeatherBox({ weatherData }) {
         </h2>
         <h3>
           현재 온도:
-          <span className="temp">
-            {(weatherData?.main.temp - 273.15).toFixed(2)} °C
-          </span>
+          <span className="temp">{weatherData?.temperature.toFixed(2)} °C</span>
         </h3>
-        <h3>
-          체감 온도:
-          <span className="temp">
-            {(weatherData?.main.feels_like - 273.15).toFixed(2)}°C
-          </span>
-        </h3>
-        <h4>
-          습도:<span className="temp">{weatherData?.main.humidity}%</span>
-        </h4>
-        <p>
-          구름 분포도:
-          <span className="temp">{weatherData?.clouds.all}%</span>
-        </p>
+        <img scr={weatherData?.weatherIconUrl} alt="weather icon" />
       </div>
     </div>
   );
